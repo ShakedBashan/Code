1fdc1871867a947c2f7e487ad995be5d73f5a506
ShakedBashan <123545134+ShakedBashan@users.noreply.github.com>
2023-04-16 16:14:09 +0300

    Add files via upload

--- chain_contactMap.py
+++ chain_contactMap.py
@@ -1,8 +1,8 @@
 #%% Imports, CSVs and dictionary setups------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# Created by Shaked on 08.03.2023
-# Last changed on 12.03.2023
-# Shows differences between contacts of chains
+"""Created by Shaked on 08.03.2023
+Last changed on 16.04.2023
+Shows differences between contacts of chains"""
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import math
 
-# Read CSVs for chains
+"""Read CSVs for chains"""
 chainA = pd.read_csv('/home_c/shaked/ftsk/chainA/chainAdf.csv')
 chainB = pd.read_csv('/home_c/shaked/ftsk/chainB/chainBdf.csv')
 chainC = pd.read_csv('/home_c/shaked/ftsk/chainC/chainCdf.csv')
@@ -20,19 +20,21 @@ chainD = pd.read_csv('/home_c/shaked/ftsk/chainD/chainDdf.csv')
 chainE = pd.read_csv('/home_c/shaked/ftsk/chainE/chainEdf.csv')
 chainF = pd.read_csv('/home_c/shaked/ftsk/chainF/chainFdf.csv')
 
-# Create dictionaries for chains
+"""Create dictionaries for chains"""
 dicA = chainA.set_index(['i', 'j'])['Distance'].to_dict()
 dicB = chainB.set_index(['i', 'j'])['Distance'].to_dict()
 dicC = chainC.set_index(['i', 'j'])['Distance'].to_dict()
 dicD = chainD.set_index(['i', 'j'])['Distance'].to_dict()
 dicE = chainE.set_index(['i', 'j'])['Distance'].to_dict()
 dicF = chainF.set_index(['i', 'j'])['Distance'].to_dict()
+
 print('length of dicA - ' + str(len(dicA)))
 print('length of dicB - ' + str(len(dicB)))
 print('length of dicC - ' + str(len(dicC)))
 print('length of dicD - ' + str(len(dicD)))
 print('length of dicE - ' + str(len(dicE)))
 print('length of dicF - ' + str(len(dicF)))
+
 # print(dicF)
 #%% Functions
 
@@ -62,69 +64,76 @@ def map_changed_contacts(dic1, dic2):
         in same_contacts.items()}
     threshold = 0.2
     significant_differences = {k: values for k, values in same_contacts_sqrt.items() \
-        if abs(values[0] - values[1]) > threshold * (values[0] + values[1]) / 2}
+        if abs(values[0] - values[1]) >= threshold * (values[0] + values[1]) / 2}
     changed_i, changed_j = zip(*significant_differences.keys())
     plt.scatter(changed_i, changed_j, s=1, color = 'black')
-    print('Contacts in common to both chains that have changed > 20%: ', len(significant_differences))
+    print('Contacts in common to both chains that have changed >= 20%: ', len(significant_differences))
     plt.show()
     return significant_differences
 
-#%% Code
-"""Map differences between chains:"""
+#%% Show a few maps
+
+"""Map differences between C and F"""
 map_only(dicC, dicF)
-
-"""Map common contacts between chains:"""
-map_commons(dicC, dicF)
-
-"""Map common contacts that changed > 20%:"""
+map_only(dicF, dicC)
+"""Map common contacts that changed > 20% between C and F"""
 changedCF = map_changed_contacts(dicC, dicF)
+print('Contacts changed >= 20%:\n', changedCF)
 print(changedCF)
 
+"""Map common contacts between C and F"""
+# map_commons(dicC, dicF)
 
-"""Create maps that can be shown together"""
+#%% Unique contacts in each chain (that are not in F)
+
+"""Create maps that can be shown together (NOT IN F)"""
 only_chainA = {(k): [dicA[k]] for k in dicA if k not in dicF}
 # print(only_chainA)
-print('Chain A unique contacts:', len(only_chainA))
+print('Chain A unique contacts:(not in F)', len(only_chainA))
 Ai, Aj = zip(*only_chainA.keys())
 plt.scatter(Ai, Aj, s=1, color = 'blue')
-plt.show()
+# plt.show()
 
 only_chainB = {(k): [dicB[k]] for k in dicB if k not in dicF}
 # print(only_chainB)
-print('Chain B unique contacts:', len(only_chainB))
+print('Chain B unique contacts:(not in F)', len(only_chainB))
 Bi, Bj = zip(*only_chainB.keys())
 plt.scatter(Bi, Bj, s=1, color = 'cornflowerblue')
-plt.show()
+# plt.show()
 
 only_chainC = {(k): [dicC[k]] for k in dicC if k not in dicF}
 # print(only_chainC)
-print('Chain C unique contacts:', len(only_chainC))
+print('Chain C unique contacts:(not in F)', len(only_chainC))
 Ci, Cj = zip(*only_chainC.keys())
 plt.scatter(Ci, Cj, s=1, color = 'lime')
-plt.show()
+# plt.show()
 
 only_chainD = {(k): [dicD[k]] for k in dicD if k not in dicF}
 # print(only_chainD)
-print('Chain D unique contacts:', len(only_chainD))
+print('Chain D unique contacts:(not in F)', len(only_chainD))
 Di, Dj = zip(*only_chainD.keys())
 plt.scatter(Ci, Cj, s=1, color = 'gold')
-plt.show()
+# plt.show()
 
 only_chainE = {(k): [dicE[k]] for k in dicE if k not in dicF}
-print(only_chainE)
-print('Chain E unique contacts:', len(only_chainE))
+# print(only_chainE)
+print('Chain E unique contacts:(not in F)', len(only_chainE))
 Ei, Ej = zip(*only_chainE.keys())
 plt.scatter(Ei, Ej, s=1, color = 'darkorange')
-plt.show()
+# plt.show()
 
 only_chainF = {(k): [dicF[k]] for k in dicF if k not in dicC}
-print(only_chainF)
-print('Chain F unique contacts:', len(only_chainF))
+# print(only_chainF)
+print('Chain F unique contacts:(not in C)', len(only_chainF))
 Fi, Fj = zip(*only_chainF.keys())
 plt.scatter(Fi, Fj, s=1, color = 'red')
-plt.show()
+# plt.show()
+
+"""*************************************************************************************"""
 
 """Show unique chain contacts on the same plot:"""
+
+"""*************************************************************************************"""
 # plt.scatter(Ai, Aj, s=1, color = 'blue')
 # plt.scatter(Bi, Bj, s=1, color = 'cornflowerblue')
 plt.scatter(Ci, Cj, s=1, color = 'lime')
@@ -132,13 +141,7 @@ plt.scatter(Ci, Cj, s=1, color = 'lime')
 # plt.scatter(Ei, Ej, s=1, color = 'darkorange')
 plt.scatter(Fi, Fj, s=1, color = 'red')
 plt.show()
-#%% Runs
 
-changedCF = map_changed_contacts(dicC, dicF)
-print(changedCF)
-
-map_only(dicC, dicF)
-map_only(dicF, dicC)
 #%% OLD DRAFTS
 
 # def differences_from_dictionaries(dic1, dic2):
@@ -183,8 +186,5 @@ map_only(dicF, dicC)
 #     plt.xlabel('i')
 #     plt.ylabel('j')
 #     return plt.show()
-
-
-
 
 #%% END
